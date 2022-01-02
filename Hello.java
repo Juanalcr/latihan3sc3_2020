218a40a8daed1071297e41c2381569f994755712
Juana Lucia Raharusun <90247533+Juanalcr@users.noreply.github.com>
2022-01-03 00:30:48 +0700

    Fourth Commit

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public static void main(String[] args){
 System.out.println("Judul");
 System.out.println("RMN: Aplikasi Java 2022");
 System.out.println("--- *** ---");
+  System.out.println("END");
 }
 }
