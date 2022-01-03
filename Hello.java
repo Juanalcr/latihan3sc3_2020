56f969f49f822810d71cda949662665f71324c9c
azmirach <ozen.kawi@gmail.com>
2022-01-03 17:42:49 +0700

    sixth commit

--- Hello.java
+++ Hello.java
@@ -4,5 +4,6 @@ System.out.println("Judul");
 System.out.println("RMN: Aplikasi Java 2022");
 System.out.println("--- *** ---");
   System.out.println("END");
+   System.out.println("JUANALCR");
 }
 }
